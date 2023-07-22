238e8cf80828519e80463bf1a9e4210f613e99f8
Yogesh Panchal <yogesh.panchal@growexx.com>
2023-07-22 22:58:10 +0530

    login with google and web scraping with nodejs

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
 "use strict";
 
-var express = require('express'),
-    request = require('request'),
-    cheerio = require('cheerio'),
-    route = express.Router(),
-    app = express(),
-    port = process.env.PORT || 8002;
+const axios = require("axios");
+const cheerio = require("cheerio");
+var express = require('express');
+var route = express.Router();
+var app = express();
+var port = process.env.PORT || 8002;
 
+/**
+ * Function to fetch webscrapers data from URL
+ * @createdDate 22-07-2023
+ * @author Yogesh
+ */
 route.get('/scraper', function (req, res) {
-    request("https://www.amazon.com/s?srs=5286335011", function (err, resp, body) {
-        if (!err && resp.statusCode === 200) {
-            var $ = cheerio.load(body),
-                productsData = [];
-
-            $('.a-size-base-plus a-color-base a-text-normal').map(function (i, data) {
-                var productName = $(data).text(),
-                    productLink = $(data).attr('href');
-
-                productsData.push({
-                    prodlink: productLink,
-                    prodName: productName
+    try {
+        const fetchWebScrapers = async () => {
+            try {
+                const response = await axios.get('https://www.amazon.com/s?srs=5286335011');
+                const html = response.data;
+                const $ = cheerio.load(html);
+                const productsData = [];
+                $('div.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.sg-col-4-of-20').each((_index, element) => {
+                    const shelf = $(element);
+                    const title = shelf.find('span.a-size-base-plus.a-color-base.a-text-normal').text();
+                    const image = shelf.find('img.s-image').attr('src');
+                    const price = shelf.find('span.a-price > span.a-offscreen').text();
+                    let prodsInfo = {
+                        title,
+                        image,
+                        price,
+                    }
+                    productsData.push(prodsInfo);
                 });
-            });
+                return productsData;
+            } catch (error) {
+                throw error;
+            }
+        };
 
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(productsData));
-        }
-    });
+        fetchWebScrapers().then((productsData) => console.log(productsData));
+    } catch (error) {
+        throw error;
+    }
 });
 app.use('/', route);
 app.listen(port);
